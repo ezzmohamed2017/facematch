131d47042518ba1e82b0089ed244f971a69419e9
ezzmohamed2017 <ezz.mohamed@gmail.com>
2018-09-10 22:03:50 +0200

    Update face.py

--- face.py
+++ face.py
@@ -1,7 +1,5 @@
 from keras import backend as K
 import time
-from multiprocessing.dummy import Pool
-K.set_image_data_format('channels_first')
 import cv2
 import os
 import glob
@@ -9,42 +7,13 @@ import numpy as np
 from numpy import genfromtxt
 import tensorflow as tf
 from fr_utils import *
-#from inception_blocks_v2 import *
 
 PADDING = 50
-ready_to_detect_identity = True
 
-FRmodel = faceRecoModel(input_shape=(3, 96, 96))
+from matplotlib.pyplot import imshow
+import face_recognition
 
-def triplet_loss(y_true, y_pred, alpha = 0.3):
-    """
-    Implementation of the triplet loss as defined by formula (3)
-    
-    Arguments:
-    y_pred -- python list containing three objects:
-            anchor -- the encodings for the anchor images, of shape (None, 128)
-            positive -- the encodings for the positive images, of shape (None, 128)
-            negative -- the encodings for the negative images, of shape (None, 128)
-    
-    Returns:
-    loss -- real number, value of the loss
-    """
-    
-    anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
-    
-    # Step 1: Compute the (encoding) distance between the anchor and the positive, you will need to sum over axis=-1
-    pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)), axis=-1)
-    # Step 2: Compute the (encoding) distance between the anchor and the negative, you will need to sum over axis=-1
-    neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)), axis=-1)
-    # Step 3: subtract the two previous distances and add alpha.
-    basic_loss = tf.add(tf.subtract(pos_dist, neg_dist), alpha)
-    # Step 4: Take the maximum of basic_loss and 0.0. Sum over the training examples.
-    loss = tf.reduce_sum(tf.maximum(basic_loss, 0.0))
-    
-    return loss
-
-FRmodel.compile(optimizer = 'adam', loss = triplet_loss, metrics = ['accuracy'])
-load_weights_from_FaceNet(FRmodel)
+%matplotlib inline
 
 def prepare_database():
     database = {}
@@ -52,60 +21,48 @@ def prepare_database():
     # load all the images of individuals to recognize into the database
     for file in glob.glob("images/*"):
         identity = os.path.splitext(os.path.basename(file))[0]
-        database[identity] = img_path_to_encoding(file, FRmodel)
+        
+        img = cv2.imread(file)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img=resize(img)
+        '''
+        if 'ezz_rot' in identity:
+            img= allignFace(img)
+            print('ezz_rot')
+        ''' 
 
+        database[identity] = face_recognizer(img)
+        print(identity)
+        #imshow(img)
+
+        
     return database
 
 
-def process_frame(img, frame, face_cascade):
+        
+def face_recognizer(frame):
     """
-    Determine whether the current frame contains the faces of people from our database
+    Runs a loop that extracts images from the computer's webcam and determines whether or not
+    it contains the face of a person in our database.
+
+    If it contains a face, an audio message will be played welcoming the user.
+    If not, the program will process the next frame from the webcam
     """
-    global ready_to_detect_identity
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    # Loop through all the faces detected and determine whether or not they are in the database
-    identities = []
-    for (x, y, w, h) in faces:
-        x1 = x-PADDING
-        y1 = y-PADDING
-        x2 = x+w+PADDING
-        y2 = y+h+PADDING
+    #frame= allignFace(frame)
 
-        img = cv2.rectangle(frame,(x1, y1),(x2, y2),(255,0,0),2)
+    boxes = face_recognition.face_locations(frame,model='cnn')
+    if boxes ==[]:
+        boxes=[(0,frame.shape[0],frame.shape[1],0)]
+        #print('empty')    
+    #print(boxes)
+    encoding = face_recognition.face_encodings(frame, boxes)      
 
-        identity = find_identity(frame, x1, y1, x2, y2)
+    return encoding  
 
-        if identity is not None:
-            identities.append(identity)
 
-    if identities != []:
-        cv2.imwrite('example.png',img)
-
-        ready_to_detect_identity = False
-        pool = Pool(processes=1) 
-        # We run this as a separate process so that the camera feedback does not freeze
-        pool.apply_async(welcome_users, [identities])
-    return img
-
-def find_identity(frame, x1, y1, x2, y2):
-    """
-    Determine whether the face contained within the bounding box exists in our database
-
-    x1,y1_____________
-    |                 |
-    |                 |
-    |_________________x2,y2
-
-    """
-    height, width, channels = frame.shape
-    # The padding is necessary since the OpenCV face detector creates the bounding box around the face and not the head
-    part_image = frame[max(0, y1):min(height, y2), max(0, x1):min(width, x2)]
-    
-    return who_is_it(part_image, database, FRmodel)
-
-def who_is_it(image, database, model):
+#database = prepare_database()
+def match_face(identity_match,encoding, database):
     """
     Implements face recognition for the happy house by finding who is the person on the image_path image.
     
@@ -118,18 +75,22 @@ def who_is_it(image, database, model):
     min_dist -- the minimum distance between image_path encoding and the encodings from the database
     identity -- string, the name prediction for the person on image_path
     """
-    encoding = img_to_encoding(image, model)
     
     min_dist = 100
-    identity = None
+
     
     # Loop over the database dictionary's names and encodings.
     for (name, db_enc) in database.items():
-        
+        #results = face_recognition.compare_faces([db_enc], encoding)
         # Compute L2 distance between the target "encoding" and the current "emb" from the database.
-        dist = np.linalg.norm(db_enc - encoding)
+        #print('db_enc',db_enc)
+        #print('encoding',encoding)
+        
+        dist = np.linalg.norm(db_enc[0] - encoding[0])
+        
+        print('%s  %s  %s' %(identity_match,name,1.0/dist))
 
-        print('distance for %s is %s' %(name, dist))
+        #print('distance for %s is %s: %s' %(name,1.0/dist, dist))
 
         # If this distance is less than the min_dist, then set min_dist to dist, and identity to name
         if dist < min_dist:
@@ -143,7 +104,6 @@ def who_is_it(image, database, model):
 
 def welcome_users(identities):
     """ Outputs a welcome audio message to the users """
-    global ready_to_detect_identity
     welcome_message = 'Welcome '
 
     if len(identities) == 1:
@@ -154,19 +114,37 @@ def welcome_users(identities):
         welcome_message += 'and %s, ' % identities[-1]
         welcome_message += 'have a nice day!'
 
-    windows10_voice_interface.Speak(welcome_message)
+        
+def resize(img):
+    height, width = img.shape[:2] # without channel
+    #print(height, width)
+    area= height* width
+    #print(area)
+    scale = 100000.0/area
+    dim=(int(width*scale),int(height*scale)) 
+    #print(dim)
+    resized = cv2.resize(img, dim, interpolation =cv2.INTER_AREA)
+    imshow(resized) 
+    return resized
+database = prepare_database()
 
-    # Allow the program to start detecting identities again
-    ready_to_detect_identity = True
+#load image
+for file in glob.glob("match/*"):
+    identity = os.path.splitext(os.path.basename(file))[0]
+            
+    img = cv2.imread(file)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img=resize(img)
+    '''
+       if 'ezz_rot' in identity:
+        img= allignFace(img)
+        print('ezz_rot')
+    '''
 
-if __name__ == "__main__":
-    database = prepare_database()
-    webcam_face_recognizer(database)
+    #database[identity] = face_recognizer(img)
+ 
+    #print(' ',identity,'Head')#,img.shape)
+    encoding= face_recognizer(img)
+    #print('encod',encoding.shape)
 
-# ### References:
-# 
-# - Florian Schroff, Dmitry Kalenichenko, James Philbin (2015). [FaceNet: A Unified Embedding for Face Recognition and Clustering](https://arxiv.org/pdf/1503.03832.pdf)
-# - Yaniv Taigman, Ming Yang, Marc'Aurelio Ranzato, Lior Wolf (2014). [DeepFace: Closing the gap to human-level performance in face verification](https://research.fb.com/wp-content/uploads/2016/11/deepface-closing-the-gap-to-human-level-performance-in-face-verification.pdf) 
-# - The pretrained model we use is inspired by Victor Sy Wang's implementation and was loaded using his code: https://github.com/iwantooxxoox/Keras-OpenFace.
-# - Our implementation also took a lot of inspiration from the official FaceNet github repository: https://github.com/davidsandberg/facenet 
-# 
+    identity= match_face(identity,encoding,database)
