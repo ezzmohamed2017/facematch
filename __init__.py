2caa17cbcd2163011b46948a9e5b8082fbbb4fbf
ezzmohamed2017 <ezz.mohamed@gmail.com>
2018-09-16 08:40:36 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,12 @@
-from flask import Flask, url_for
+#!/bin/usr python
+
+from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def api_root():
-    return 'Welcome'
-
-@app.route('/articles')
-def api_articles():
-    return 'List of ' + url_for('api_articles')
-
-@app.route('/articles/<articleid>')
-def api_article(articleid):
-    return 'You are reading ' + articleid
+def index():
+    return 'Welcome first restAPI'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
